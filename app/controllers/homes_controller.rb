ebb9ad37677411fdc20b4b0fb5423b05cda3646b
miyaokat <miyaokta1234@gmail.com>
2019-08-26 11:58:10 +0000

    [Edit]マイページ、ユーザー退会機能を作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def top
-  	@photos = Photo.where(public_flag: true).order("RANDOM()").limit(16)
+  	@photos = Photo.where(public_flag: true).order("RANDOM()").limit(10)
   end
 end
